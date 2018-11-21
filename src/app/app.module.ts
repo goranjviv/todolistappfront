66be6195029447da49c24bae71b3ca6e895f1b8e
Goran <goran.jankovic@vivifyideas.com>
2018-11-21 10:10:30 +0100

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponentComponent } from './login-component/login-component.component';
+import { RegisterComponentComponent } from './register-component/register-component.component';
+import { TaskListComponentComponent } from './task-list-component/task-list-component.component';
+import { TodoDetailsComponentComponent } from './todo-details-component/todo-details-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponentComponent,
+    RegisterComponentComponent,
+    TaskListComponentComponent,
+    TodoDetailsComponentComponent
   ],
   imports: [
     BrowserModule,
